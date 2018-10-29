7369ae964ff3d7463fc7cca40fa71d192ce23ccd
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2018-10-30 00:51:46 +0800

    update example

--- example/multi_process_web_server.cpp
+++ example/multi_process_web_server.cpp
@@ -4,7 +4,6 @@
 #include <mongols/util.hpp>
 #include <mongols/web_server.hpp>
 #include <iostream>
-#include <algorithm>
 
 
 static void signal_cb(int sig, siginfo_t *, void *);
@@ -12,20 +11,6 @@ static std::vector<pid_t> pids;
 
 int main(int, char**) {
     //    daemon(1, 0);
-
-    std::vector<int> sigs = {SIGHUP, SIGTERM, SIGINT, SIGQUIT};
-    struct sigaction act;
-    for (auto& i : sigs) {
-        memset(&act, 0, sizeof (struct sigaction));
-        sigemptyset(&act.sa_mask);
-        act.sa_sigaction = signal_cb;
-        act.sa_flags = SA_SIGINFO;
-        if (sigaction(i, &act, NULL) < 0) {
-            perror("sigaction error");
-            return -1;
-        }
-    }
-
     auto f = [](const mongols::request & req) {
         if (req.method == "GET" && req.uri.find("..") == std::string::npos) {
             return true;
@@ -48,42 +33,44 @@ int main(int, char**) {
                 server.run(f);
             }
     , pids);
+    
     for (int i = 0; i < child_process_len; ++i) {
         mongols::process_bind_cpu(pids[i], i);
     }
 
+    const int sig_len = 4;
+    int sigs[sig_len] = {SIGHUP, SIGTERM, SIGINT, SIGQUIT};
+    struct sigaction act;
+    memset(&act, 0, sizeof (struct sigaction));
+    sigemptyset(&act.sa_mask);
+    act.sa_sigaction = signal_cb;
 
-    for (size_t i = 0; i < pids.size(); ++i) {
-        pid_t pid;
-        int status;
-        if ((pid = wait(&status)) > 0) {
-            if (WIFSIGNALED(status)) {
-                std::vector<int>::iterator p = std::find(pids.begin(), pids.end(), pid);
-                if (p != pids.end()) {
-                    *p = -1 * pid;
-                }
-                if (std::find(sigs.begin(), sigs.end(), WTERMSIG(status)) == sigs.end())
-                    mongols::forker(1
-                        , [&]() {
-                            server.run(f);
-                        }
-                , pids);
-            }
+    for (int i = 0; i < sig_len; ++i) {
+        if (sigaction(sigs[i], &act, NULL) < 0) {
+            perror("sigaction error");
+            return -1;
         }
     }
 
+
+
+    for (size_t i=0;i<pids.size();++i) {
+        pid_t pid;
+        if ((pid = wait(NULL)) > 0) {
+
+        }
+    }
+    
 }
 
-static void signal_cb(int sig, siginfo_t *, void *) {
+static void signal_cb(int sig, siginfo_t *, void * ) {
     switch (sig) {
         case SIGTERM:
         case SIGHUP:
         case SIGQUIT:
         case SIGINT:
             for (auto & i : pids) {
-                if (i > 0) {
-                    kill(i, SIGTERM);
-                }
+                kill(i, SIGTERM);
             }
             break;
         default:break;
