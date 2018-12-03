d990b9031439a217a7153bc332d741aa275a32a9
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2018-12-03 19:51:18 +0800

    update example

--- example/html/lua/index.lua
+++ example/html/lua/index.lua
@@ -3,9 +3,9 @@ local echo=require('echo')
 mongols_res:header('Content-Type','text/plain;charset=UTF-8')
 mongols_res:content(echo.concat('hello,','world'))
 mongols_res:status(200)
---]]
-
 --
+
+--[[
 
 local echo=require('echo')
 local route=require('route'):get_instance()
