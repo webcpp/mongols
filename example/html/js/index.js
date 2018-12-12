2a0234b42bfe302a01d0122d6fe54f72da6c0aba
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2018-12-12 10:39:39 +0800

    update  example

--- example/html/js/index.js
+++ example/html/js/index.js
@@ -1,4 +1,13 @@
+/*
+var math = require('math/math')
+var v=math.minus(1,3)/math.add(100,3)
+mongols_res.header('Content-Type','text/plain;charset=UTF-8')
+mongols_res.content(v.toString())
+mongols_res.status(200)
+*/
+
 var foo = require('foo')
 mongols_res.header('Content-Type','text/plain;charset=UTF-8')
 mongols_res.content(foo.hello())
 mongols_res.status(200)
+
