f43aba64f524defe1bfa5ab8cbc1efd5b4584d0c
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2018-12-13 23:06:50 +0800

    update js_server and example

--- example/html/js/index.js
+++ example/html/js/index.js
@@ -21,12 +21,12 @@ mongols_res.content(loaded?adder(1,2).toString():'failed load c module.')
 mongols_res.status(200)
 */
 
-///*
+/*
 var loaded = mongols_module.require('concat/libconcat','concat')
 mongols_res.header('Content-Type','text/plain;charset=UTF-8')
 mongols_res.content(loaded?concat('Hello,','world'):'failed load c module.')
 mongols_res.status(200)
-//*/
+*/
 
 /*
 var mustache = require('mustache')
@@ -67,8 +67,8 @@ mongols_res.status(200)
 */
 
 
-/*
+///*
 mongols_res.header('Content-Type','text/plain;charset=UTF-8')
 mongols_res.content('hello,world')
 mongols_res.status(200)
-*/
+//*/
