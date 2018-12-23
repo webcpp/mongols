61d4503f5e63e2fbe118ab97e99e806076c3900f
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2018-12-23 15:33:45 +0800

    update example

--- example/html/lua/jsonrpc.lua
+++ example/html/lua/jsonrpc.lua
@@ -1,13 +1,13 @@
 -- curl -i -X POST -H 'Content-Type: application/json' --data '{"method":"post"}'  http://127.0.0.1:9090/jsonrpc.lua
-local json=require('JSON')
+local cjson=require('cjson')
 
 if mongols_req:has_form('__body__') 
 then
-    local body = json:decode(mongols_req:get_form('__body__'))
+    local body = cjson.decode(mongols_req:get_form('__body__'))
     local result={}
     result['jsonrpc']=2.0
     result['method']=body['method']
-    mongols_res:content(json:encode(result))
+    mongols_res:content(cjson.encode(result))
     mongols_res:status(200)
     mongols_res:header('Content-Type','application/json')
 else
