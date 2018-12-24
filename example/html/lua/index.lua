573f3c42cfe505fcfce1c9356c026910c5e4d548
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2018-12-24 14:02:08 +0800

    update example

--- example/html/lua/index.lua
+++ example/html/lua/index.lua
@@ -5,6 +5,13 @@ mongols_res:content(echo.concat('hello,','world'))
 mongols_res:status(200)
 --
 
+--[[
+local m = require('hello')
+mongols_res:header('Content-Type','text/plain;charset=UTF-8')
+mongols_res:content(m.hello())
+mongols_res:status(200)
+--]]
+
 
 --[[
 
---
+++ example/html/lua/package/hello/src/hello.cpp
@@ -0,0 +1,21 @@
+#include <mongols/lib/lua/kaguya.hpp>
+
+std::string hello() {
+    return "hello cpp module";
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    int luaopen_hello(lua_State* L) {
+        kaguya::State state(L);
+        kaguya::LuaTable module = state.newTable();
+        module["hello"] = kaguya::function(&hello);
+        return module.push();
+    }
+
+#ifdef __cplusplus
+}
+#endif
+
