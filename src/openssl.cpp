ae36926b3d8fb3e76dfd9a14f39e0525fbef1f82
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2019-01-16 08:55:04 +0800

    update  openssl , tcp_proxy_server and example

--- src/openssl.cpp
+++ src/openssl.cpp
@@ -6,7 +6,7 @@
 namespace mongols {
 
     openssl::version_t openssl::version = openssl::version_t::TLSv12;
-    std::string openssl::ciphers = "AES128-GCM-SHA256";
+    std::string openssl::ciphers = "ECDHE-ECDSA-AES128-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:RSA+AES128:!aNULL:!eNULL:!LOW:!ADH:!RC4:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS"; //"AES128-GCM-SHA256";
     long openssl::flags = SSL_OP_NO_COMPRESSION | SSL_OP_CIPHER_SERVER_PREFERENCE | SSL_OP_SINGLE_ECDH_USE | SSL_OP_SINGLE_DH_USE;
     bool openssl::enable_verify = false, openssl::enable_cache = false;
     size_t openssl::cache_size = 128;
