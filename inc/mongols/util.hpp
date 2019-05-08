381c35651d5b15859b450030f0eb4a9d23ccd257
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2019-05-08 20:16:16 +0800

    update

--- inc/mongols/util.hpp
+++ inc/mongols/util.hpp
@@ -1,104 +1,100 @@
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
-
-#include <pthread.h>
-#include <functional>
 #include <algorithm>
-#include <ctime>
-#include <cstdlib>
 #include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <pthread.h>
 #include <string>
+#include <thread>
 #include <unordered_map>
 #include <vector>
-#include <thread>
-
 
 namespace mongols {
 
+std::string random_string(const std::string& s);
 
-    std::string random_string(const std::string& s);
+bool read_file(const std::string& path, std::string& out);
 
-    bool read_file(const std::string& path, std::string& out);
+bool is_file(const std::string& s);
 
-    bool is_file(const std::string& s);
+bool is_dir(const std::string& s);
 
-    bool is_dir(const std::string& s);
+std::string http_time(time_t* t);
 
+time_t parse_http_time(u_char* value, size_t len);
 
-    std::string http_time(time_t *t);
+std::string trim(const std::string& s);
+void trim(std::string& s);
 
-    time_t parse_http_time(u_char* value, size_t len);
+void parse_param(const std::string& data, std::unordered_map<std::string, std::string>& result, char c = '&', char cc = '=');
 
-    std::string trim(const std::string& s);
-    void trim(std::string& s);
+void split(const std::string& s, char delim, std::vector<std::string>& v);
+void split(const std::string& s, const std::string& delim, std::vector<std::string>& v);
+std::vector<std::string> split(const std::string& s, char delimiter);
 
-    void parse_param(const std::string& data, std::unordered_map<std::string, std::string>& result, char c = '&', char cc = '=');
+class random {
+public:
+    random()
+        : now(time(0))
+    {
+        srand(this->now);
+    }
 
-    void split(const std::string& s, char delim, std::vector<std::string>& v);
-    void split(const std::string& s, const std::string& delim, std::vector<std::string>& v);
-    std::vector<std::string> split(const std::string& s, char delimiter);
+    template <class T>
+    T create(T left, T right)
+    {
+        return static_cast<T>(this->create() * (right - left) + left);
+    }
 
-    class random {
-    public:
+private:
+    time_t now;
 
-        random() : now(time(0)) {
-            srand(this->now);
-        }
+    double create()
+    {
+        return static_cast<double>(rand() / (RAND_MAX + 1.0));
+    }
+};
 
-        template<class T>
-        T create(T left, T right) {
-            return static_cast<T> (this->create()*(right - left) + left);
-        }
-    private:
-        time_t now;
+struct regular_expression {
+    static std::string INTEGER, NUMBER, EMAIL, URL;
+};
 
-        double create() {
-            return static_cast<double> (rand() / (RAND_MAX + 1.0));
-        }
-    };
+std::string base64_encode(const std::string&);
+std::string base64_decode(const std::string&);
 
-    struct regular_expression {
-        static std::string INTEGER, NUMBER, EMAIL, URL;
-    };
+std::string bin2hex(const std::string&);
 
+std::string url_encode(const std::string& str);
 
-    std::string base64_encode(const std::string&);
-    std::string base64_decode(const std::string&);
+std::string url_decode(const std::string& str);
 
+pid_t forker(int, const std::function<void()>&, std::vector<std::pair<pid_t, int>>&);
 
-    std::string bin2hex(const std::string&);
+bool process_bind_cpu(pid_t pid, int cpu);
 
-    std::string url_encode(const std::string& str);
+bool regex_match(const std::string& pattern, const std::string& str, std::vector<std::string>& results);
 
-    std::string url_decode(const std::string& str);
+bool regex_find(const std::string& pattern, const std::string& str, std::vector<std::string>& results);
 
-    pid_t forker(int, const std::function<void()>&, std::vector<std::pair<pid_t, int> >&);
+class multi_process {
+public:
+    multi_process();
 
-    bool process_bind_cpu(pid_t pid, int cpu);
+    virtual ~multi_process();
 
-    bool regex_match(const std::string & pattern, const std::string & str, std::vector<std::string> & results);
+    void run(const std::function<void(pthread_mutex_t*, size_t*)>& f, const std::function<bool(int)>& g, size_t = std::thread::hardware_concurrency());
 
-    bool regex_find(const std::string & pattern, const std::string & str, std::vector<std::string> & results);
-
-    class multi_process {
-    public:
-        multi_process();
-
-        virtual~multi_process();
-
-        void run(const std::function<void(pthread_mutex_t*, size_t*)>& f, const std::function<bool(int) >& g,size_t=std::thread::hardware_concurrency() );
-
-
-    private:
-        pthread_mutex_t *mtx;
-        pthread_mutexattr_t *mtx_attr;
-        size_t *data;
-        static std::vector<std::pair<pid_t, int>> pids;
-        static void signal_cb(int sig);
-        static void set_signal();
-    };
+private:
+    pthread_mutex_t* mtx;
+    pthread_mutexattr_t* mtx_attr;
+    size_t* data;
+    static std::vector<std::pair<pid_t, int>> pids;
+    static void signal_cb(int sig);
+    static void set_signal();
+};
 }
 
 #endif /* UTIL_HPP */
-
