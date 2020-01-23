50f7ee0a626380035bf3e88d57b47c7a7de46dbe
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2020-01-23 23:00:55 +0800

    update example

--- example/html/qjs/index.js
+++ example/html/qjs/index.js
@@ -2,7 +2,24 @@ import * as std from "std";
 import * as os from "os";
 import * as bjson from "bjson";
 import * as mongols from "mongols";
+import Mustache from "./mustache.mjs"
+
+
+/*
+var view = {
+    "musketeers": ["Athos", "Aramis", "Porthos", "D'Artagnan"]
+  };
+
+var tpl = "{{#musketeers}}* {{.}}{{/musketeers}}";
+
+var content = Mustache.render(tpl, view);
 
 mongols.status(200);
-mongols.header('Content-Type', 'text/plain');
-mongols.content("hello,world\n");
+mongols.header('Content-Type', 'text/html;charset=utf-8');
+mongols.content(content);
+*/
+
+
+mongols.status(200);
+mongols.header('Content-Type', 'text/plain;charset=utf-8');
+mongols.content('hello,world\n');
