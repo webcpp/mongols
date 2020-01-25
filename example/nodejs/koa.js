26e88dc7a4921fde8112ccf81cc1391c2c8631fb
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2020-01-25 23:23:31 +0800

    update example

---
+++ example/nodejs/koa.js
@@ -0,0 +1,23 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const Koa = require('koa');
+const route = require('koa-route');
+const app = new Koa();
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+} else {
+
+  app.use(route.get('/', ctx => {
+    ctx.response.body = 'hello,world\n';
+  }));
+
+  app.listen(3000);
+
+}
--- example/nodejs/test.js
+++ example/nodejs/test.js
@@ -13,9 +13,6 @@ if (cluster.isMaster) {
     cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
 } else {
 
   const server = http.createServer((req, res) => {
