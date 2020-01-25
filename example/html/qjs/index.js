739f1ead082b7764ca44680bc706a39f0bc1fee2
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2020-01-25 22:12:14 +0800

    update example

--- example/html/qjs/index.js
+++ example/html/qjs/index.js
@@ -1,9 +1,10 @@
 //import * as std from "std";
 //import * as os from "os";
 //import * as bjson from "bjson";
-import * as mongols from "mongols";
 //import * as hash from "hash";
+import * as mongols from "mongols";
 //import Mustache from "./mustache.mjs"
+//import route from "./route.mjs"
 
 
 /*
@@ -20,6 +21,23 @@ mongols.header('Content-Type', 'text/html;charset=utf-8');
 mongols.content(content);
 */
 
+/*
+var r = route.get_instance();
+
+r.get('^\/', function (m, param) {
+  m.status(200);
+  m.header('Content-Type', 'text/plain;charset=utf-8');
+  m.content('hello,world\n');
+});
+
+r.get('^\/get/?(\\w+)$', function (m, param) {
+  m.header('Content-Type', 'text/plain;charset=UTF8')
+  m.content(m.method() + '\n' + m.uri() + '\n' + param.toString())
+  m.status(200)
+});
+
+r.run(mongols)
+*/
 
 mongols.status(200);
 mongols.header('Content-Type', 'text/plain;charset=utf-8');
