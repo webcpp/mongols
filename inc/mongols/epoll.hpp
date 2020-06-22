a3a588269ce2fd1922de05295b6c454df3f80bac
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2020-06-22 15:10:31 +0800

    formated

--- inc/mongols/epoll.hpp
+++ inc/mongols/epoll.hpp
@@ -1,34 +1,30 @@
 #ifndef EPOLL_HPP
 #define EPOLL_HPP
 
-#include <sys/epoll.h>
 #include <functional>
-
+#include <sys/epoll.h>
 
 namespace mongols {
 
-    class epoll {
-    public:
+class epoll {
+    epoll() = delete;
+    epoll(int ev_size = 64, int timeout = -1);
 
-        epoll() = delete;
-        epoll(int ev_size = 64, int timeout = -1);
+    virtual ~epoll();
 
-        virtual~epoll();
+public:
+    bool is_ready() const;
+    bool add(int fd, int event);
+    bool del(int fd);
+    bool mod(int fd, int event);
+    size_t size() const;
+    size_t expires() const;
+    void loop(const std::function<void(struct epoll_event*)>& handler);
 
-    public:
-
-        bool is_ready()const;
-        bool add(int fd, int event);
-        bool del(int fd);
-        bool mod(int fd, int event);
-        size_t size()const;
-        size_t expires()const;
-        void loop(const std::function<void(struct epoll_event*) >& handler);
-    private:
-        int epfd, ev_size, real_ev_size, timeout;
-        struct epoll_event ev, *evs;
-    };
+private:
+    int epfd, ev_size, real_ev_size, timeout;
+    struct epoll_event ev, *evs;
+};
 }
 
 #endif /* EPOLL_HPP */
-
