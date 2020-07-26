4eea7dfb3441c852df3e455dfd773bd3adbac8be
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2020-07-26 08:15:46 +0800

    update example

--- example/tcp_proxy_server.cpp
+++ example/tcp_proxy_server.cpp
@@ -20,12 +20,12 @@ int main(int, char**)
     int port = 9090;
     const char* host = "127.0.0.1";
 
-    mongols::tcp_proxy_server server(host, port, 5000, 8192, 0 /*2*/);
+    mongols::tcp_proxy_server server(host, port, 5000, 8192, 0);
 
-    server.set_enable_tcp_send_to_other(true);
+    server.set_enable_tcp_send_to_other(false);
     //see example/nodejs
-    server.set_backend_server(host, 8886);
-    server.set_backend_server(host, 8887);
+    server.set_backend_server(host, 8888);
+    server.set_backend_server(host, 8889);
     //    if (!server.set_openssl("openssl/localhost.crt", "openssl/localhost.key")) {
     //        return -1;
     //    }
