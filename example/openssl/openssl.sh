a99718f3f3b606103f025228df559d6797bfa2d0
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2020-08-14 06:09:15 +0800

    update example

--- example/openssl/openssl.sh
+++ example/openssl/openssl.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 openssl req -x509 -out localhost.crt -keyout localhost.key \
-  -newkey rsa:2048 -nodes -sha256 \
+  -newkey rsa:2048 -nodes -sha256 -days 3650\
   -subj '/CN=localhost' -extensions EXT -config openssl.config
