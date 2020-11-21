5518bbec4877e85b3f4d6f38409c9843e0b86d1e
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2020-11-21 11:48:12 +0800

    update example

--- example/tcp_server_benchmark.cpp
+++ example/tcp_server_benchmark.cpp
@@ -1,39 +1,40 @@
-#include <unistd.h>
+#include <algorithm>
+#include <ctime>
 #include <iostream>
 #include <memory>
-#include <string>
-#include <vector>
-#include <thread>
-#include <ctime>
-#include <algorithm>
-#include <mutex>
-#include <mongols/tcp_proxy_server.hpp>
 #include <mongols/lib/args.hxx>
+#include <mongols/tcp_proxy_server.hpp>
 #include <mongols/thread_pool.hpp>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <unistd.h>
+#include <vector>
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     args::ArgumentParser parser("This is a tcp server benchmark program.", "This goes after the options.");
-    args::HelpFlag help(parser, "help", "Display this help menu",{'h', "help"});
-    args::ValueFlag<std::string> host(parser, "host", "The host flag",{"host"});
-    args::ValueFlag<int> port(parser, "port", "The port flag",{"port"});
-    args::ValueFlag<int> client(parser, "client", "The client flag",{"client"});
-    args::ValueFlag<int> buffer(parser, "buffer", "The buffer flag",{"buffer"});
-    args::ValueFlag<int> loop(parser, "loop", "The loop flag",{"loop"});
-    args::ValueFlag<int> ssl(parser, "ssl", "The ssl flag",{"ssl"});
+    args::HelpFlag help(parser, "help", "Display this help menu", { 'h', "help" });
+    args::ValueFlag<std::string> host(parser, "host", "The host flag", { "host" });
+    args::ValueFlag<int> port(parser, "port", "The port flag", { "port" });
+    args::ValueFlag<int> client(parser, "client", "The client flag", { "client" });
+    args::ValueFlag<int> buffer(parser, "buffer", "The buffer flag", { "buffer" });
+    args::ValueFlag<int> loop(parser, "loop", "The loop flag", { "loop" });
+    args::ValueFlag<int> ssl(parser, "ssl", "The ssl flag", { "ssl" });
 
     std::string _host = "localhost";
     int _port = 9090, _client = 100, _buffer = 4096, _loop = 1000;
     bool _ssl = false;
     try {
         parser.ParseCLI(argc, argv);
-    } catch (args::Help) {
+    } catch (args::Help&) {
         std::cout << parser;
         return 0;
-    } catch (args::ParseError e) {
+    } catch (args::ParseError& e) {
         std::cerr << e.what() << std::endl;
         std::cerr << parser;
         return -1;
-    } catch (args::ValidationError e) {
+    } catch (args::ValidationError& e) {
         std::cerr << e.what() << std::endl;
         std::cerr << parser;
         return -1;
@@ -63,9 +64,7 @@ int main(int argc, char** argv) {
     std::cout << "loop: " << _loop << std::endl;
     std::cout << "ssl: " << std::boolalpha << _ssl << std::endl;
 
-
-
-    std::vector<std::shared_ptr < mongols::tcp_client>> cli;
+    std::vector<std::shared_ptr<mongols::tcp_client>> cli;
 
     std::string str(_buffer, 'c');
 
@@ -73,8 +72,7 @@ int main(int argc, char** argv) {
         cli.emplace_back(std::make_shared<mongols::tcp_client>(_host, _port, _ssl));
     }
 
-
-    mongols::thread_pool < std::function<void(void) >> th_pool;
+    mongols::thread_pool<std::function<void(void)>> th_pool;
 
     for (int i = 0; i < _client; ++i) {
         th_pool.submit([&, i, _loop]() {
@@ -91,13 +89,5 @@ int main(int argc, char** argv) {
         });
     }
 
-
-
-
     return 0;
 }
-
-
-
-
-
